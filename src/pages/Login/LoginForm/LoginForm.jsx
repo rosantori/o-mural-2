b310d6dc6093724eb8045808e56922df701e70ed
rosantori <rosanasrib@gmail.com>
2022-01-11 17:13:50 -0300

    feat: add password field and sign in sing up buttons

--- src/pages/Login/LoginForm/LoginForm.jsx
+++ src/pages/Login/LoginForm/LoginForm.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { TextField } from '@mui/material';
+import { Button, Grid, TextField } from '@mui/material';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { loginFormSchema } from 'validators/loginFormSchema';
 
 function LoginForm() {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit, errors } = useForm({
     resolver: yupResolver(loginFormSchema),
   });
 
@@ -21,21 +22,63 @@ function LoginForm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit, onError)}>
-      <Controller
-        name="username"
-        control={control}
-        render={({ field: { onChange }}) => (
-          <TextField 
-            defaultValue=""
-            value={username}
-            onChange={(e) => {
-              const { value } = e.target;
-              onChange(value);
-              setUsername(value);
-            }}
+      <Grid container direction="column" spacing={2}>
+        <Grid item>
+          <Controller
+            name="username"
+            control={control}
+            render={({ field: { onChange }}) => (
+              <TextField 
+                defaultValue=""
+                value={username}
+                label="Usuário"
+                placeholder="Digite o usuário"
+                onChange={(e) => {
+                  const { value } = e.target;
+                  onChange(value);
+                  setUsername(value);
+                }}
+                error={errors?.username}
+                helperText={errors?.username?.message}
+              />
+            )}
           />
-        )}
-      />
+        </Grid>
+        <Grid item>
+          <Controller
+            name="password"
+            control={control}
+            render={({ field: { onChange }}) => (
+              <TextField 
+                defaultValue=""
+                placeholder="Digite sua senha"
+                label="Senha"
+                value={password}
+                type="password"
+                onChange={(e) => {
+                  const { value } = e.target;
+                  onChange(value);
+                  setPassword(value);
+                }}
+                error={errors?.password}
+                helperText={errors?.password?.message}
+              />
+            )}
+          />
+        </Grid>
+        <Grid item container spacing={2}>
+          <Grid item>
+            <Button variant="contained">
+              Sign in
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained">
+              Sign up
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
       
     </form>
   );
--- src/validators/loginFormSchema.js
+++ src/validators/loginFormSchema.js
@@ -2,6 +2,9 @@ import * as yup from 'yup';
 
 const loginFormSchema = yup.object({
   username: yup.string().required('Campo necessário'),
+  password: yup.string()
+    .min(6, "Campo necessário")
+    .required('Campo necessário'),
 }).required();
 
 export { loginFormSchema };
