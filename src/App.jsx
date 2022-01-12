f452a55ebf50de2e957c2d12ddfc39524fe396b5
rosantori <rosanasrib@gmail.com>
2022-01-11 23:05:38 -0300

    feat: adiciona toastify e mui/lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import { Routes } from 'routes';
 
 function App() {
   return (
+    <>
+      <ToastContainer/> 
       <Routes />
+    </>
   );
 }
 
