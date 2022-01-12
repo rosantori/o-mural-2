b8ab132500ebddb987c4f0af16191d4d477431e1
rosantori <rosanasrib@gmail.com>
2022-01-11 21:32:26 -0300

    style: cria css para Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from 'assets/logo-completa.svg';
+import './Home.css';
 
 function Home() {
   return (
-    <img src={logo} alt="o Mural" />
+    <div className="home-container">
+      <img className="logo" src={logo} alt="o Mural" />
+    </div>
   );
 }
 
